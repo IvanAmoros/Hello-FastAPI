dc8d991626b5b91cb39c3515fcc8508e013405c6
iamoros <ivan13ap@gmail.com>
2024-07-10 00:47:08 +0200

    docs

--- routers/hotel.py
+++ routers/hotel.py
@@ -11,18 +11,7 @@ import requests
 
 router = APIRouter()
 
-@router.get("/test-connection")
-def test_connection():
-    try:
-        response = requests.get("https://www.booking.com/")
-        if response.status_code == 200:
-            return {"status": "success", "code": response.status_code}
-        else:
-            return {"status": "failed", "code": response.status_code}
-    except Exception as e:
-        return {"status": "failed", "error": str(e)}
-
-@router.post("/hotels/scrape", response_model=HotelResponse)
+@router.post("/hotels/scrape", response_model=HotelResponse, tags=["Scrape"])
 def scrape_and_create_hotel(hotel_request: HotelScrapeRequest, db: Session = Depends(get_db)):
     try:
         hotel_details = scrape_hotel_details(hotel_request.name)
@@ -46,38 +35,53 @@ def scrape_and_create_hotel(hotel_request: HotelScrapeRequest, db: Session = Dep
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
 
-@router.post("/hotels/", response_model=HotelResponse)
+@router.post("/hotels/", response_model=HotelResponse, tags=["Create"])
 def create_hotel_view(hotel: HotelCreate, db: Session = Depends(get_db)):
     return create_hotel(db=db, hotel=hotel)
 
-@router.get("/hotels/", response_model=List[HotelResponse])
+@router.get("/hotels/", response_model=List[HotelResponse], tags=["Read"])
 def read_hotels(db: Session = Depends(get_db)):
     return get_hotels(db=db)
 
-@router.get("/hotels/hotel_id/{hotel_id}", response_model=HotelResponse)
+@router.get("/hotels/hotel_id/{hotel_id}", response_model=HotelResponse, tags=["Read"])
 def read_hotel(hotel_id: int, db: Session = Depends(get_db)):
     db_hotel = get_hotel_by_id(db=db, hotel_id=hotel_id)
     if db_hotel is None:
         raise HTTPException(status_code=404, detail=f"Hotel with id {hotel_id} not found")
     return db_hotel
 
-@router.get("/hotels/name/{hotel_name}", response_model=HotelResponse)
+@router.get("/hotels/name/{hotel_name}", response_model=HotelResponse, tags=["Read"])
 def read_hotel(hotel_name: str, db: Session = Depends(get_db)):
     db_hotel = get_hotel_by_name(db=db, name=hotel_name)
     if db_hotel is None:
         raise HTTPException(status_code=404, detail=f"Hotel with name {hotel_name} not found")
     return db_hotel
 
-@router.put("/hotels/{hotel_id}", response_model=HotelResponse)
+@router.put("/hotels/{hotel_id}", response_model=HotelResponse, tags=["Update"])
 def update_hotel_view(hotel_id: int, hotel_update: HotelUpdate, db: Session = Depends(get_db)):
     db_hotel = update_hotel(db=db, hotel_id=hotel_id, hotel_update=hotel_update)
     if db_hotel is None:
         raise HTTPException(status_code=404, detail=f"Hotel with id {hotel_id} not found")
     return db_hotel
 
-@router.delete("/hotels/{hotel_id}", response_model=HotelResponse)
+@router.delete("/hotels/{hotel_id}", response_model=HotelResponse, tags=["Delete"])
 def delete_hotel_view(hotel_id: int, db: Session = Depends(get_db)):
     db_hotel = delete_hotel(db=db, hotel_id=hotel_id)
     if db_hotel is None:
         raise HTTPException(status_code=404, detail=f"Hotel with id {hotel_id} not found")
     return db_hotel
+
+@router.get("/", tags=["Other"])
+def read_root():
+    return {"message": "Welcome to the Hotel Scraper API!"}
+
+@router.get("/test-connection", tags=["Other"])
+def test_connection():
+    try:
+        response = requests.get("https://www.booking.com/")
+        if response.status_code == 200:
+            return {"status": "success", "code": response.status_code}
+        else:
+            return {"status": "failed", "code": response.status_code}
+    except Exception as e:
+        return {"status": "failed", "error": str(e)}
