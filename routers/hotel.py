fe29aabd43a1d0ddfce2310246bce15daefbace1
iamoros <ivan13ap@gmail.com>
2024-07-09 14:08:59 +0200

    test router

--- routers/hotel.py
+++ routers/hotel.py
@@ -7,7 +7,20 @@ from crud.hotel import create_hotel, get_hotels, get_hotel, get_hotel_by_name
 from database import get_db
 from scraper import scrape_hotel_details
 
+import requests
+
 router = APIRouter()
+
+@router.get("/test-connection")
+def test_connection():
+    try:
+        response = requests.get("https://www.booking.com/")
+        if response.status_code == 200:
+            return {"status": "success", "code": response.status_code}
+        else:
+            return {"status": "failed", "code": response.status_code}
+    except Exception as e:
+        return {"status": "failed", "error": str(e)}
 
 @router.post("/hotels/scrape", response_model=HotelResponse)
 def scrape_and_create_hotel(hotel_request: HotelScrapeRequest, db: Session = Depends(get_db)):
